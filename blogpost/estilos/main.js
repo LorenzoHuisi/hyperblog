32484eed4c23cd8fc6aa09bd58c3ce60f1573a9f
lorenzo <lorenzo.huisi@gmail.com>
2022-06-09 15:06:50 -0300

    nueva lines agregada desde vim

---
+++ blogpost/estilos/main.js
@@ -0,0 +1,23 @@
+
+
+// let lorem = "esta es una frace espica que voy a renderizar con run code \n" 
+// + "este texto va abajo"
+
+// let loren2 = `texto del loren2 que no necesita un backslash para poder poner
+
+// abajo mas texto`
+
+
+// console.log(lorem)
+// console.log(loren2)
+
+let lorenzo = {
+    name: "lorenzo",
+    age: 26,
+    job: "programer"
+}
+
+console.log(lorenzo.name + " es mi nombre,", "tengo " + lorenzo.age + " años")
+
+let {name} = lorenzo;
+console.log(name)
